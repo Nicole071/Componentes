64a9960cffb8e45cb31591accf4a82c05e23c608
Nicole071 <79389031+Nicole071@users.noreply.github.com>
2021-04-10 22:47:27 -0500

    Add files via upload

---
+++ 2DO CORTE/Componentes/js/script.js
@@ -0,0 +1,13 @@
+/* Activadpr de jQuery */
+$(document).ready(function(){
+    $('.slick-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaysppeed: 3000,
+        speed: 300,
+        arrows: true,
+        fade: true,
+        pauseOnHover: false
+    });
+});
